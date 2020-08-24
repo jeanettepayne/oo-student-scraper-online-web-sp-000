ae1695e76d3b64229eb68f3dfcedbf6398d435f6
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-24 22:39:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,7 +12,7 @@ class Scraper
       binding.pry
       {:name => attributes.css("h4").text,
         :location => attributes.css("p").text,
-        :profile_url => attributes.css("a").href
+        :profile_url => attributes.css("a").attr
       }
     end
   end
