126f87c2d5413cd8a37526cb8d0454542e2e2bfa
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-24 22:25:20 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,20 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    doc = Nokogiri::HTML(open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html"))
+    student_info_card = doc.css("div .student-card")
     
+    student_info_card.collect do |attributes|
+      # binding.pry
+      {:name => attributes.css("h4").text,
+        :location => attributes.css("p").text,
+        :profile_url => attributes.css("a").text
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
@@ -13,3 +23,8 @@ class Scraper
 
 end
 
+
+#all info - div class student-card
+# name - div card-text-container -> h4 class student-name
+#location - div card-text-container -> p class student-location
+#profile url - a href
