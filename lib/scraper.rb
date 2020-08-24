fe6b192865bf0c19d049f5731e1fe2b015616690
Jeanette Payne <jeanetterpayne@gmail.com>
2020-08-24 23:44:43 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -35,10 +35,10 @@ class Scraper
         end
       end
     
-    social_links[:profile_quote] = doc.css(".vitals-text-container .profile-quote").text
-    social_links[:bio] = doc.css(".description-holder").text
+    info_hash[:profile_quote] = doc.css(".vitals-text-container .profile-quote").text
+    info_hash[:bio] = doc.css(".description-holder").text
     
-    social_links
+    info_hash
   end
 
 end
